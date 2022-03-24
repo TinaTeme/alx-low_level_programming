71b108be21401c50b8994f483946fffb20d66da0
TinaTeme <Tinebebt216@gmail.com>
2022-03-24 13:23:55 -0700

    My first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+*main - Entry point
+*My hello. printf task
+*Return: return 1 fter program excution
+*/
+int main(void)
+{
+printf("with proper grammar. but the out come is a piece of art.\n");
+return (0);
+}
